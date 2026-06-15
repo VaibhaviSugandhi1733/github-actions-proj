047d77f2a6bcba648a4c99cc5d600aa2d08437f5
VaibhaviSugandhi1733 <118549468+VaibhaviSugandhi1733@users.noreply.github.com>
2026-06-15 23:36:29 +0530

    Simplify health endpoint and improve formatting
    Refactor health endpoint response and clean up code.

--- app.py
+++ app.py
@@ -4,26 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
     return "DevOps CI/CD Pipeline Working!"
-
 
 @app.route("/health")
 def health():
+    return {"status": "healthy"}
 
-    return {
-
-        "status":"healthy"
-
-    }
-
-
-if __name__=="__main__":
-
-    app.run(
-
-        host="0.0.0.0",
-
-        port=5000
-
-    )
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
