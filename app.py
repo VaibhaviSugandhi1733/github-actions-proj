7cb1062b478558113dede49134346e72a5b67c02
VaibhaviSugandhi1733 <118549468+VaibhaviSugandhi1733@users.noreply.github.com>
2026-06-17 21:13:24 +0530

    Update environment identifier in home route

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Return the environment identifier."""
-    return "Dev Environment ClarityTTS by Vaibhavi123"
+    return "Dev Environment ClarityTTS by Vaibhavi"
 
 
 @app.route("/health")
